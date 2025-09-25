e3ce6613a8841a2246754d5186c076f83477b10a
Nicolas Gioanni <nicolasmgioanni@gmail.com>
2025-09-24 21:10:27 -0700

    Add searchMatrix solution for 2D matrix search
    Implemented binary search to find target in a 2D matrix.

--- Problems/Search a 2D Matrix/solution.py
+++ Problems/Search a 2D Matrix/solution.py
@@ -1,1 +1,29 @@
-# TODO: implement solution for Search a 2D Matrix
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        rowT, rowB = 0, len(matrix) - 1
+        colL, colR = 0, len(matrix[0]) - 1
+
+        while rowT <= rowB:
+            rowM = (rowT + rowB) // 2
+
+            if target > matrix[rowM][colR]:
+                rowT = rowM + 1
+            elif target < matrix[rowM][colL]:
+                rowB = rowM - 1
+            else:
+                
+                while colL <= colR:
+                    colM = (colL + colR) // 2
+
+                    if target > matrix[rowM][colM]:
+                        colL = colM + 1
+                    elif target < matrix[rowM][colM]:
+                        colR = colM - 1
+                    else:
+                        return True
+        return False
